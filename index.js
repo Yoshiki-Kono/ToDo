f3b907a4fb9e7ccb8454110113bca01c818f0dd5
Yoshiki-Kono <kono.yoshiki-gb-sense.com>
2020-05-20 10:56:57 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const addButton=document.getElementById(`add`);
+    addButton.onclick=function({
+    refreshIndex();
+    const priorityValue=document.schedule_data.priority.value;
+    const scheduleName=document.schedule_data.schedule.value;
+    const limitName=document.schedule_data.limit.value;
+    const table=document.getElementById(`ToDo`);
+    let row 
+    })
