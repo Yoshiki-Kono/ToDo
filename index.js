6b31c81638bfb51607f79e530cbe0b6273ec1732
Yoshiki-Kono <kono.yoshiki-gb-sense.com>
2020-05-21 17:07:04 +0900

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function submitItem() {
-    var todoItems = [];
-    var todoItem = document.getElementById('item').value;
-    todoItems.push(todoItem);
-    for (var i =0; i < todoItems.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = todoItems[i]
-      document.getElementById('todoList').appendChild(li);
-    }
-  }
+`use strict`;
+
+const addToDoTask=document.getElementsByClassName(`ToDoTask`);
+const completerButton=document.getElementById(`complete`);
+const schduleForm=document.getElementById(`schedule`);
+const limitForm=document.getElementById(`limit`);
+
+const Task=deleteButton=> {
+    const plusTask=deleteButton.closest(`li`);
+    scheduleForm.limitForm.removeChild(Task);
+};
+
+const plusTask=Task=> {
+    const taskObj=document.createElement(`li`)
+    const deleteButton=document.createElement(`Button`);
+    
+    deleteButton.innerText=`削除`;
+    deleteButton.addEventListener(`click`,()=>deleteTask(deleteButton));
+
+    taskObj.innerText=Task;
+
+    taskObj.append(deleteButton);
+    completerButton.appendChild(taskObj);
+};
+
