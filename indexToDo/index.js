893883ca114740821fdcaaffe01e5af2238f0bb4
Yoshiki-Kono <kono.yoshiki-gb-sense.com>
2020-05-24 12:40:17 +0900

    Go first commit

---
+++ indexToDo/index.js
@@ -0,0 +1,73 @@
+window.onload = getTodoList();
+
+const completeButton = document.getElementById('complete');
+const scheduleForm = document.getElementById('schedule');
+const limitForm = document.getElementById('limit');
+
+
+//DBの方へ登録するデータを送る
+completeButton.onclick = function() {
+    const form = {};
+    form.scheduleForm = scheduleForm.value;
+    form.limitform = limitForm.value;
+    const JsonData = JSON.stringify(form);
+    fetch("/register",{
+        method : "post",
+        body : JsonData
+    }).then(response => {
+        if (response.status === 200) {
+            alert('test');
+            getTodoList();
+        }
+    });
+};
+
+//DBから表示する為のデータを受け取る
+function getTodoList() {
+    fetch("/display")
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      }).then(todoList => {
+        const table = document.getElementById(`tourokuyotei`);
+        while (table.rows[0]) {
+          table.deleteRow(0);
+        }
+        todoList.forEach(function(element) {
+        let schedule = element.scheduleForm;
+        let timeLimit = element.limitForm;
+        let id = element.id;
+        let row = table.insertRow(-1);
+        let cell = row.insertCell(-1);
+        let text = document.createTextNode(schedule);
+        cell.appendChild(text);
+        cell = row.insertCell(-1);
+        text = document.createTextNode(timeLimit);
+        cell.appendChild(text);
+        cell = row.insertCell(-1);
+        let button = document.createElement("button");
+        button.type = "button";
+        button.value = id;
+        button.innerText = "削除";
+        cell.appendChild(button);
+        button.addEventListener("click", deleteTodo, false);
+          });
+        });
+}
+
+//DBから削除したデータを受け取る
+function deleteTodo(event) {
+    const val = event.target.vulue;
+    const form = { id : val };
+    const JsonData = JSON.stringify(form);
+    fetch("/deleteTodo" , {
+        method : "post",
+        body : JsonData
+    }).then(response => {
+        if (response.status === 200) {
+              alert("削除");
+              getTodoList();
+            };
+    });
+};
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type todoList []todo
+
+var db *sql.DB
+
+//構造体一覧
+type todo struct {
+	ID        string `db:"id" json:"id"`
+	Schedule  string `db:"schedule" json:"schedule"`
+	TimeLimit string `db:"timelimit" json:"timeLimit"`
+}
+
+func connect() (db *sql.DB, err error) {
+	dbConn, err := sql.Open("mysql", "root:0111@/todo")
+	if err != nil {
+		log.Println(err)
+	}
+	return dbConn, err
+}
+
+func main() {
+	db, err := connect()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	db = db
+	http.Handle("/", http.FileServer(http.Dir("todo")))
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/display", display)
+	http.HandleFunc("/remove", remove)
+	log.Fatal(http.ListenAndServe(":8880", nil))
+}
+
+//予定登録時の処理
+func register(w http.ResponseWriter, r *http.Request) {
+	form := todo{}
+	db, err := sql.Open("mysql", "root:0111@/todo")
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	schedule := form.Schedule
+	timeLimit := form.TimeLimit
+
+	stmt, err := db.Prepare("INSERT INTO trn_todo(schedule, timelimit) VALUES(?,?)")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(schedule, timeLimit)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
+
+//DBの内容を一覧表示させる
+func display(w http.ResponseWriter, r *http.Request) {
+	todoList := todoList{}
+	db, err := sql.Open("mysql", "root:0111@/todo")
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, schedule, timelimit FROM trn_todo")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	for rows.Next() {
+		todo := todo{}
+		err := rows.Scan(&todo.ID, &todo.Schedule, &todo.TimeLimit)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		todoList = append(todoList, todo)
+	}
+	defer rows.Close()
+
+	JSONTodoList, err := json.Marshal(todoList)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(JSONTodoList)
+}
+
+func remove(w http.ResponseWriter, r *http.Request) {
+	form := todo{}
+
+	db, err := sql.Open("mysql", "root:0111@/todo")
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	targetID := form.ID
+
+	stmt, err := db.Prepare("DELETE FROM trn_todo WHERE id = ?")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(targetID)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
