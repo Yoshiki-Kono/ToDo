0db5616a14c57a186059dc4610d3eeb493fa13e5
Yoshiki-Kono <kono.yoshiki-gb-sense.com>
2020-05-26 17:13:42 +0900

    コードの改良中(元コードはコメントアウト)

--- indexToDo/index.js
+++ indexToDo/index.js
@@ -1,3 +1,97 @@
+const table = document.getElementById("table")
+
+function makeTr(data) {
+  const tr = document.createElement("tr")
+
+  for (h of data) {
+    if (h == "button") {
+      const bt = document.createElement("button")
+      bt.textContent = "削除"
+      bt.onclick = function() {
+        const id = data[0]
+
+        fetch(`/todos?id=${id}`, {
+          method: 'DELETE',
+        }).then((response) => {
+          if (response.ok) {
+            alert("削除しました")
+            updateTable()
+          } else {
+            alert("エラー")
+          }
+        }).catch((err) => {
+          console.log(err);
+        })
+      }
+      tr.appendChild(bt)
+    } else {
+      const td = document.createElement("td")
+      td.textContent = h
+      tr.appendChild(td)
+    }
+  }
+
+  return tr
+}
+
+function makeHeader() {
+  const headers = ["id", "schedule", "limit", "operation"]
+  return makeTr(headers)
+}
+
+function getTodo() {
+  fetch("/todos").then((response) => {
+    return response.json();
+  }).then((todos) => {
+    for (todo of todos) {
+      let t = Object.values(todo)
+      t.push("button")
+      table.appendChild(makeTr(t))
+    }
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+function createTodo() {
+  const schedule = document.getElementById("schedule")
+  const limit = document.getElementById("limit")
+
+  const form = {
+    Schedule: schedule.value,
+    Limit: limit.value,
+  }
+
+  fetch("/todos", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(form),
+  }).then(() => {
+    alert("登録しました")
+    updateTable()
+    schedule.value = ""
+    limit.value = ""
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+function deleteTodo() {
+
+}
+
+function updateTable() {
+    table.innerHTML = ""
+    table.appendChild(makeHeader())
+    getTodo()
+}
+
+table.appendChild(makeHeader())
+getTodo()
+
+/*
 window.onload = getTodoList();
 
 const completeButton = document.getElementById('complete');
@@ -70,3 +164,4 @@ function deleteTodo(event) {
             };
     });
 };
+*/
